145a98f222474b333c4e254295ef7dc34c764508
Theshakyhorse <amaximus.sierra@gmail.com>
2026-03-01 18:46:36 -0800

    second

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 //COMSC 210 | Lab 15 | Alexander Sierra
 #include <iostream>
+#include <iomanip>
+#include <vector>
+#include <fstream>
 using namespace std;
+
+const int W = 15, SIZE = 4;
+
+class Movie{
+private:
+    string writer;
+    int year;
+    string title;
+public:
+    string getWriter() {return writer;}
+    void setWriter(string w) {writer = w;}
+    int getYear() {return year;}
+    void setYear(int y) {year = y;}
+    string getTitle() {return title;}
+    void setTitle(string t) {title = t;}
+
+    void print() {
+        cout << setw(W) << "Movie: " << title << endl;
+        cout << setw(W) << "Year released: " << year << endl;
+        cout << setw(W) << "Screenwriter: " << writer << endl << endl;
+    }
+};
 
 int main() {
 
